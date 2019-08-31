f3822e5d9545a9b542ebc3ddb960adc061f82ab3
Jose Pedron Tessele <josetesselep@gmail.com>
2019-08-30 22:13:41 -0300

    Initial Commit

---
+++ Whitehouse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Whitehouse
+//
+//  Created by José Eduardo Pedron Tessele on 30/08/19.
+//  Copyright © 2019 José P Tessele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
